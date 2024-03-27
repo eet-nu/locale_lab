9e00436b0a561d53daf0afd9031ff313f34d2e55
Tom-Eric Gerritsen <ik@tom-eric.info>
2024-03-27 08:34:30 +0100

    Add initial `dashboard#show` action that does some interacting with an application’s translation files

--- app/controllers/locale_lab/dashboard_controller.rb
+++ app/controllers/locale_lab/dashboard_controller.rb
@@ -1,7 +1,6 @@
 module LocaleLab
   class DashboardController < ApplicationController
     def show
-      render plain: 'locale_lab'
     end
   end
 end
--- lib/locale_lab/engine.rb
+++ lib/locale_lab/engine.rb
@@ -1,5 +1,9 @@
 module LocaleLab
   class Engine < ::Rails::Engine
     isolate_namespace LocaleLab
+
+    initializer "locale_lab.assets.precompile" do |app|
+      app.config.assets.precompile += %w[ locale_lab/application.css ]
+    end
   end
 end
