2feac1a5138d6f76249ab4edebae372392b05bf2
Tom-Eric Gerritsen <ik@tom-eric.info>
2024-11-13 16:05:38 +0100

    Add ‘Copy to clipboard’ for keys

--- app/javascript/locale_lab/controllers/translation_key_controller.js
+++ app/javascript/locale_lab/controllers/translation_key_controller.js
@@ -2,12 +2,27 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
 
-  static targets = ['form']
+  static targets = ['form', 'input']
 
   static classes = ['hidden']
 
   connect() {
     console.log('connected')
+  }
+
+  copyKeyToClipboard (event) {
+    if (this.hasInputTarget) {
+      if (event) {
+        event.preventDefault()
+      }
+
+      if (navigator.clipboard) {
+        navigator.clipboard.writeText(this.inputTarget.value)
+      }
+
+      this.inputTarget.focus()
+      this.inputTarget.select()
+    }
   }
 
   toggleForm() {
