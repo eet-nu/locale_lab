182a10f5227900a8da249ef2001eb12e34304d65
Michiel Meertens <meertensmichiel28@gmail.com>
2024-11-14 19:57:50 +0100

    Removed console.log

--- app/javascript/locale_lab/controllers/translation_key_controller.js
+++ app/javascript/locale_lab/controllers/translation_key_controller.js
@@ -6,10 +6,6 @@ export default class extends Controller {
 
   static classes = ['hidden']
 
-  connect() {
-    console.log('connected')
-  }
-
   copyKeyToClipboard (event) {
     if (this.hasInputTarget) {
       if (event) {
