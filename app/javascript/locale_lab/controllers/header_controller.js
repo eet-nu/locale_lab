a67a059f38c0ae7a2b92322b52ae1d0da3263710
Michiel Meertens <meertensmichiel28@gmail.com>
2024-11-22 14:25:37 +0100

    Set the current position as value in the form, autofocus at the end of the input

--- app/javascript/locale_lab/controllers/header_controller.js
+++ app/javascript/locale_lab/controllers/header_controller.js
@@ -2,10 +2,16 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
 
-  static targets = ['newTranslationDialog']
+  static targets = ['newTranslationDialog', 'input']
 
   showCreateForm() {
     this.newTranslationDialogTarget.showModal()
+
+    this.inputTarget.focus();
+    this.inputTarget.setSelectionRange(
+      this.inputTarget.value.length,
+      this.inputTarget.value.length
+    );
   }
 
   hideCreateForm() {
