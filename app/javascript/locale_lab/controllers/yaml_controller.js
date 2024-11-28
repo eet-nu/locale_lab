d30277921f84801285b0a77acd8c4d952a91c0b3
Michiel Meertens <meertensmichiel28@gmail.com>
2024-11-28 10:49:59 +0100

    Removed duplicate method

--- app/javascript/locale_lab/controllers/yaml_controller.js
+++ app/javascript/locale_lab/controllers/yaml_controller.js
@@ -4,19 +4,9 @@ export default class extends Controller {
   static outlets = ['editor']
 
   static values = {
-    yaml: { type: String, default: '' },
+    yaml:   { type: String, default: '' },
     locale: String,
     action: String
-  }
-
-  editInEditor() {
-    this.editorOutlet.show(this.yamlValue, this.editorOutlet.yaml)
-    this.editorOutlet.onSave((new_value) => {
-      // TODO: Save the new value
-      console.log(new_value)
-    });
-
-
   }
 
   editInEditor() {
