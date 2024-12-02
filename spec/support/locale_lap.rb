d6fd33adb9be7719c4f26d614690e9fe5cc01e6c
Michiel Meertens <meertensmichiel28@gmail.com>
2024-12-02 15:24:34 +0100

    Added `LocaleLabTestHelper` that restores the yamls after each test

--- spec/support/locale_lap.rb
+++ spec/support/locale_lap.rb
@@ -1,7 +1,29 @@
 require 'spec_helper'
 
+module LocaleLabTestHelper
+  def self.restore_backup
+    @original_i18n_files.each do |file|
+      File.write(file[:path], file[:content])
+    end
+  end
+
+  def self.take_backup
+    @original_i18n_files = LocaleLab::TranslationFile.all.map do |file|
+      { path: file.path, content: file.to_yaml }
+    end
+  end
+end
+
 RSpec.configure do |config|
+  config.before(:suite) do
+    LocaleLabTestHelper.take_backup
+  end
+
   config.before(:each) do
     Thread.current[:locale_lab] = nil
+  end
+
+  config.after(:each) do
+    LocaleLabTestHelper.restore_backup
   end
 end
